e0d3963ecd4ba3bc7d5f060dd8d7ef57f54c910f
Dimitris Apostolakis <dsapostolakis@gmail.com>
2024-02-15 01:31:23 +0200

    Reacting to Events

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,11 @@
 export default function TabButton({ label }) {
+  function handleClick() {
+    console.log(`You clicked the ${label} tab!`);
+  }
+
   return (
     <li>
-      <button>{label}</button>
+      <button onClick={handleClick}>{label}</button>
     </li>
   );
 }
